4ada7cdf435bf88e1ecc4fed138dbda28e9caf38
David Li <fantasticmrdavid@gmail.com>
2023-03-08 20:32:02 +1100

    Update site title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import type { AppProps } from "next/app"
-import {
-    QueryClient,
-    QueryClientProvider,
-} from '@tanstack/react-query'
-import { ConfigProvider, theme } from 'antd';
-import "antd/dist/reset.css"
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ConfigProvider, theme } from 'antd'
+import 'antd/dist/reset.css'
+
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    const [queryClient] = React.useState(() => new QueryClient())
+  const [queryClient] = React.useState(() => new QueryClient())
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            <ConfigProvider
-                theme={{
-                    algorithm: theme.defaultAlgorithm,
-                }}
-            >
-                <Component {...pageProps} />
-            </ConfigProvider>
-        </QueryClientProvider>
-    )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider
+        theme={{
+          algorithm: theme.defaultAlgorithm,
+        }}
+      >
+        <Head>
+          <title>CelebrateDone</title>
+        </Head>
+        <Component {...pageProps} />
+      </ConfigProvider>
+    </QueryClientProvider>
+  )
 }
