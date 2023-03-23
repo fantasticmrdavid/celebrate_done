42e3c55fc240d1099821afdea0348c40e4b0abcc
David Li <david.li@cultureamp.com>
2023-03-23 12:23:23 +1100

    Add fireworks js to done

--- pages/done/index.tsx
+++ pages/done/index.tsx
@@ -1,10 +1,13 @@
-import React, { useContext, useState } from 'react'
+import React, {useContext, useEffect, useRef, useState} from 'react'
 import { Space, Spin, Tag } from 'antd'
 import { QueryKey, useQuery } from '@tanstack/react-query'
 import { Todo, TODO_SIZE } from '@/app/components/TodoItem/types'
 import { DoneCount } from '@/app/components/DoneCount/DoneCount'
 import { sizeTags } from '@/app/components/TodoItem/Todo'
 import { UserContext } from '@/app/contexts/User'
+
+import { Fireworks } from '@fireworks-js/react'
+import type { FireworksHandlers } from '@fireworks-js/react'
 
 import styles from "./done.module.scss"
 
@@ -25,6 +28,7 @@ export const DonePage = () => {
         `/api/todos/done?user_id=${user.uuid}&date=${currentDate}`
       ).then((res) => res.json())
   )
+  const ref = useRef<FireworksHandlers>(null)
 
   if (isLoading || !todoList)
     return (
@@ -40,21 +44,36 @@ export const DonePage = () => {
       size={'middle'}
       className={styles.container}
     >
-      <DoneCount count={todoList.length} />
-      <div>
-        <h1>🎉 What I did today:</h1>
-        <ul style={{ fontSize: '1rem', lineHeight: 1.25 }}>
-          {todoList.map((t) => (
-            <li key={`todo_${t.id}`}>
-              {t.name}{' '}
-              {t.size !== TODO_SIZE.SMALL && (
-                <Tag color={sizeTags[t.size].color}>
-                  {sizeTags[t.size].label}
-                </Tag>
-              )}
-            </li>
-          ))}
-        </ul>
+      <div className={styles.bg}>
+        <Fireworks
+            ref={ref}
+            options={{ delay: { min: 100, max: 500 }}}
+            style={{
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+            }}
+        />
+      </div>
+      <div className={styles.content}>
+        <DoneCount count={todoList.length} />
+        <div>
+          <h1>🎉 What I did today:</h1>
+          <ul style={{ fontSize: '1rem', lineHeight: 1.25 }}>
+            {todoList.map((t) => (
+              <li key={`todo_${t.id}`}>
+                {t.name}{' '}
+                {t.size !== TODO_SIZE.SMALL && (
+                  <Tag color={sizeTags[t.size].color}>
+                    {sizeTags[t.size].label}
+                  </Tag>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </Space>
   )
