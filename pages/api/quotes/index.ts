560c0c14ae9611e42899be4f3df48a6edc7b1310
David Li <david.li@cultureamp.com>
2023-03-29 11:50:28 +1100

    Switch quotes category to dreams

--- pages/api/quotes/index.ts
+++ pages/api/quotes/index.ts
@@ -9,7 +9,7 @@ export const getQuotes = async (
   try {
     const options = {
       method: 'GET',
-      url: `https://api.api-ninjas.com/v1/quotes?category=imagination`,
+      url: `https://api.api-ninjas.com/v1/quotes?category=dreams`,
       headers: {
         'X-Api-Key': process.env.API_NINJA_KEY,
       }
