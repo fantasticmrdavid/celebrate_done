e71913eb4179796b711c982d988c9d5a10b207a2
David Li <david.li@cultureamp.com>
2023-08-18 14:05:04 +1000

    Convert future queries

--- pages/api/todos/future.ts
+++ pages/api/todos/future.ts
@@ -1,59 +1,67 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { dbConnect } from '@/config/dbConnect'
-import { TODO_PRIORITY, TODO_STATUS } from '@/app/components/TodoItem/utils'
-import SqlString from 'sqlstring'
-import {
-  Get_Todos_Response,
-  mapTodosResponse,
-} from '@/pages/api/todos/getTodos'
+import { TODO_STATUS } from '@/app/components/TodoItem/utils'
+import prisma from '@/app/lib/prisma'
+import { Prisma } from '@prisma/client'
+
+const todoWithCategory = Prisma.validator<Prisma.TodoDefaultArgs>()({
+  include: {
+    category: true,
+  },
+})
+
+export type TodoWithCategory = Prisma.TodoGetPayload<typeof todoWithCategory>
 
 export const getFutureTodos = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ) => {
-  const { user_id, dateRangeStart, dateRangeEnd } = req.query
-  if (!user_id) return {}
+  const { userId, dateRangeStart, dateRangeEnd } = req.query
+  if (!userId) return {}
 
   try {
-    const query = `SELECT
-        t.id,
-        t.created,
-        t.startDate,
-        t.name,
-        t.notes,
-        t.size,
-        t.priority,
-        t.status,
-        t.completedDateTime,
-        c.id AS category_id,
-        c.name AS category_name,
-        c.uuid AS category_uuid,
-        c.description AS category_description,
-        c.maxPerDay AS category_maxPerDay,
-        c.sortOrder AS category_sortOrder,
-        c.user_uuid AS category_user_uuid
-      FROM todos t
-      LEFT JOIN todos_to_categories tc ON tc.todo_uuid = t.uuid
-      LEFT JOIN categories c ON tc.category_id = c.uuid
-      WHERE
-      c.user_uuid = ? AND
-      (
-        t.status != ${SqlString.escape(TODO_STATUS.DONE)} 
-        AND t.startDate >= ? 
-        AND t.startDate <= ?
-      )
-      ORDER BY
-        t.startDate ASC,
-        (t.priority = ${SqlString.escape(TODO_PRIORITY.URGENT)}) DESC,
-        c.name, t.name ASC`
-    const results = await dbConnect.query({
-      sql: query,
-      values: [user_id, dateRangeStart, dateRangeEnd],
+    const results = await prisma.todo.findMany({
+      include: {
+        category: true,
+      },
+      where: {
+        AND: [
+          {
+            userId: {
+              equals: userId as string,
+            },
+          },
+          {
+            status: {
+              equals: TODO_STATUS.INCOMPLETE,
+            },
+          },
+          {
+            startDate: {
+              gte: dateRangeStart as string,
+              lte: dateRangeEnd as string,
+            },
+          },
+        ],
+      },
+      orderBy: [
+        {
+          startDate: 'asc',
+        },
+        {
+          priority: 'desc',
+        },
+        {
+          category: {
+            name: 'asc',
+          },
+        },
+        {
+          name: 'asc',
+        },
+      ],
     })
-    await dbConnect.end()
-    return res
-      .status(200)
-      .json(mapTodosResponse(results as Get_Todos_Response[]))
+
+    return res.status(200).json(results)
   } catch (error) {
     console.error(error)
     return res.status(500).json({ error })
